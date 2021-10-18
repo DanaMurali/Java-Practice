e3b72fa4a9599b506a3835dc0d384174cdbc458a
Darania Muralitharan <90543261+DanaMurali@users.noreply.github.com>
2021-10-18 21:09:44 +0100

    Add files via upload

--- HelloWorld/src/main/java/Main.java
+++ HelloWorld/src/main/java/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Aloha");
+        
+        int age = 20;
+        long age2 = 20L;
+
+        double pi = 3.14;
+        float pi2 = 3.14F;
+
+        char letter = 'A';
     }
 }
